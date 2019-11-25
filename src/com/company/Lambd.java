5479425ef952c18f40f693b205f5469c7a315dd9
Nef1lim23 <56215877+Nef1lim23@users.noreply.github.com>
2019-11-25 18:16:30 +0300

    Add files via upload

---
+++ src/com/company/Lambd.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Lambd
+{
+    interface Calculation
+    {
+        public boolean calculation(int value);
+    }
+
+    static boolean isPerfectSquare(int x)
+    {
+        int s = (int)Math.sqrt(x);
+        return (s*s == x);
+    }
+
+    static boolean isFibonacci(int n)
+    {
+        return isPerfectSquare(5 * n * n + 4) ||
+                isPerfectSquare(5 * n * n - 4);
+    }
+
+    public static void main(String[] args)
+    {
+        Calculation calc1 = (int value) ->
+        {
+            if (value % 15 == 0)
+            {
+                return true;
+            }
+            return false;
+        };
+
+        Calculation calc2 = (int value) ->
+        {
+            if (value >= 0)
+            {
+                return true;
+            }
+            return false;
+        };
+
+        Calculation calc3 = (int value) ->
+        {
+            if (!isFibonacci(value))
+            {
+                return true;
+            }
+            return false;
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            if (scanner.hasNextInt())
+            {
+                int value = scanner.nextInt();
+                System.out.println("value % 15 == 0 -> " + calc1.calculation(value));
+                System.out.println("value > 0 -> " + calc2.calculation(value));
+                System.out.println("value is not Fibonacci -> " + calc3.calculation(value));
+                System.out.println("-----------------------------------------------");
+            }
+            else if (scanner.hasNext("exit"))
+            {
+                return;
+            }
+            else
+            {
+                scanner.nextLine();
+            }
+        }
+    }
+}
